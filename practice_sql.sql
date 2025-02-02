f5f97aac94f803076cb238b8a13fcd3c39edd998
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-02-02 01:04:00 -0800

    Practicing Qs

--- practice_sql.sql
+++ practice_sql.sql
@@ -28,3 +28,40 @@ SELECT
 COUNT(*)
 FROM reviews
 WHERE NOT sentiment = 'Good' AND NOT sentiment = 'Bad'
+
+
+-- Select the names of customers whose names start with 'J'
+-- followed by exactly four characters
+SELECT customer_name
+FROM customer
+WHERE customer_name LIKE 'J____'
+
+
+
+-- Select the names of districts along with their populations
+-- where the district name starts with 'P' and ends with 'n'
+SELECT
+district_name, district_population
+FROM district
+WHERE district_name LIKE 'P%n'
+
+-- Find all districts with a population between 50,000 and 100,000
+SELECT *
+FROM district
+WHERE district_population BETWEEN 50000 and 100000
+
+-- Selecting name, district_id, price, minimum_nights columns
+-- from the listings table, where minimum_nights is between 3 and 7
+-- and district_id is either 5, 6, or 7, and
+-- ordering the results by minimum_nights in ascending order
+SELECT 
+name, district_id, price, minimum_nights
+FROM listings
+WHERE minimum_nights BETWEEN 3 and 7 AND district_id in (5, 6, 7)
+ORDER BY minimum_nights ASC;
+
+-- Select the top 5 districts with the largest populations
+SELECT district_name
+FROM district
+ORDER BY district_population DESC
+LIMIT 5
